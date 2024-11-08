c57b202bb3787d57883d107dac5c4489b6187896
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2024-11-08 20:04:04 -0300

    get initial data for cards
    Still unplayable for now

---
+++ src/scripts/index.js
@@ -0,0 +1,89 @@
+import { pokemonNamesArray } from "./pokemonNames.js"
+
+const cardsArray = []
+
+function getPokemonData(pokemonName) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then(response => response.json())
+        .then(data => saveData(data))
+}
+
+
+function saveData(data) {
+    const name = data.name
+    const type1 = data.types[0].type.name
+    const type2 = data.types[1] ? data.types[1].type.name : ""
+    const image = data.sprites.other["official-artwork"].front_default
+
+    return { name, type1, type2, image }
+}
+
+function randomPokemonName() {
+    const randomIndex = Math.floor(Math.random() * pokemonNamesArray.length)
+    return pokemonNamesArray[randomIndex].toLowerCase()
+}
+
+function createCard() {
+    const card = document.createElement("div")
+    card.classList.add("card")
+
+    const cardImg = document.createElement("img")
+    cardImg.classList.add("cardImg")
+
+    const cardName = document.createElement("p")
+    cardName.classList.add("cardName")
+
+    const cardTypes = document.createElement("span")
+    cardTypes.classList.add("types")
+
+    const cardType1 = document.createElement("p")
+    cardType1.classList.add("type")
+
+    const cardType2 = document.createElement("p")
+    cardType2.classList.add("type")
+
+    card.append(cardImg, cardName, cardTypes)
+    cardTypes.append(cardType1, cardType2)
+
+    return { card, cardImg, cardName, cardType1, cardType2 }
+}
+
+async function addValuesToCard({ card, cardImg, cardName, cardType1, cardType2 }) {
+    try {
+        const data = await getPokemonData(randomPokemonName())
+        console.log(data)
+
+        cardName.textContent = data.name
+        cardType1.textContent = data.type1
+        cardType1.dataset.type = data.type1
+        cardType2.textContent = data.type2
+        cardType2.dataset.type = data.type2
+        cardType2.textContent == 0 ? cardType2.classList.add("hidden") : cardType2.classList.remove("hidden")
+        cardImg.src = data.image
+
+        cardsArray.push(card)
+        cardsArray.push(card.cloneNode(true))
+    } catch (error) {
+        console.error('Error fetching data:', error)
+    }
+}
+
+async function showData() {
+    while (cardsArray.length < 16) {
+        const cardElements = createCard()
+        await addValuesToCard(cardElements)
+    }
+    
+
+    console.log(cardsArray)
+    console.log(cardsArray.length)
+    console.log(typeof(cardsArray))
+
+    const fragment = document.createDocumentFragment()
+    for (const card of cardsArray) {
+        fragment.append(card)
+    }
+    document.querySelector("main").appendChild(fragment)
+}
+
+showData()
