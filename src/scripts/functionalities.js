eced212f6a03b8224ca4e0fcfab1b99555f14cb1
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2024-11-19 17:08:58 -0300

    [style] center content

--- src/scripts/functionalities.js
+++ src/scripts/functionalities.js
@@ -7,27 +7,27 @@ document.addEventListener("cardsReady", () => {
     cards.forEach(card => {
         card.addEventListener("click", () => {
             card.classList.toggle("flipped");
-            changeBackgroundColor(card);
         });
     });
+    changeBackgroundColor(cards);
 });
 
 
-function changeBackgroundColor(card) {
-    console.log(card)
+function changeBackgroundColor(cards) {
 
-    const cardFront = card.querySelector(".card-front")
+    cards.forEach(card => {
+        const cardFront = card.querySelector(".card-front")
 
-    console.log(cardFront)
-    
-    if (!cardFront.dataset.type2) {
-        cardFront.style.backgroundImage = `linear-gradient(to top left, var(--color-${cardFront.dataset.type1}) 50%, #fff 50%)`
+        if (!cardFront.dataset.type2) {
+            cardFront.style.backgroundImage = `linear-gradient(to top left, var(--color-${cardFront.dataset.type1}) 50%, #fff 50%)`
 
-        cardFront.style.borderColor = `var(--color-${cardFront.dataset.type1}) #fff #fff var(--color-${cardFront.dataset.type1})`
-    }
-    else {
-        cardFront.style.backgroundImage = `linear-gradient(to top right, var(--color-${cardFront.dataset.type1}) 50%, var(--color-${cardFront.dataset.type2}) 50%)`
+            cardFront.style.borderColor = `var(--color-${cardFront.dataset.type1}) #fff #fff var(--color-${cardFront.dataset.type1})`
+        }
+        else {
+            cardFront.style.backgroundImage = `linear-gradient(to top right, var(--color-${cardFront.dataset.type1}) 50%, var(--color-${cardFront.dataset.type2}) 50%)`
 
-        cardFront.style.borderColor = `var(--color-${cardFront.dataset.type1}) var(--color-${cardFront.dataset.type1}) var(--color-${cardFront.dataset.type2}) var(--color-${cardFront.dataset.type2})`
-    }
+            cardFront.style.borderColor = `var(--color-${cardFront.dataset.type1}) var(--color-${cardFront.dataset.type1}) var(--color-${cardFront.dataset.type2}) var(--color-${cardFront.dataset.type2})`
+        }
+    })
+
 }
